8d9bf2939dcbe97dcafcdd00026992440e4b6058
Eckhart Koeppen <eck@40hz.org>
2013-10-22 12:22:53 +0300

    Storage module fixes

--- protocol/storage_module.go
+++ protocol/storage_module.go
@@ -15,20 +15,20 @@ type StorageModule struct {
 
 func getDefaultStore(state int, input interface{}, output interface{}, data interface{}) {
 	module := data.(*StorageModule)
-	packet := DantePacketNew(GET_DEFAULT_STORE)
+	packet := DantePacketNew(GET_DEFAULT_STORE, []byte{})
 	module.ToDockLink <- *packet
 }
 
 func gotDefaultStore(state int, input interface{}, output interface{}, data interface{}) {
-	module := data.(*StorageModule)
+	// module := data.(*StorageModule)
 }
 
 func StorageModuleNew(toDockLink chan DantePacket) *StorageModule {
 	var module StorageModule
 	module.DockModule.DockModuleInit(toDockLink, &module)
 	module.stateTable = map[int][]fsm.State{
-		fsm.ANY:           		{{Input: DantePacketCommand{APP_GET_DEFAULT_STORE}, NewState: STORAGE_GET_DEFAULT_STORE, Action: getDefaultStore}},
-		STORAGE_GET_DEFAULT_STORE:	{{Input: DantePacketCommand{APP_GET_DEFAULT_STORE}, NewState: fsm.Any, Action: gotDefaultStore}},
+		STORAGE_IDLE:          		{{Input: DantePacketCommand{APP_GET_DEFAULT_STORE}, NewState: STORAGE_GET_DEFAULT_STORE, Action: getDefaultStore}},
+		STORAGE_GET_DEFAULT_STORE:	{{Input: DantePacketCommand{APP_GET_DEFAULT_STORE}, NewState: STORAGE_IDLE, Action: gotDefaultStore}},
 	}
 	module.reader()
 	return &module
